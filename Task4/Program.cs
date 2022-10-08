eb3329d7fe52e478c5dfbfc7811444c587e764eb
Anastasya <okayota83@mail.ru>
2022-10-08 21:09:08 +0400

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.WriteLine("Введите N");
+int N = int.Parse(Console.ReadLine () ?? "0");
+int number = 1;
+double cub = 0;
+
+while (number<=N)
+{
+    if (number%2==0)
+    {
+        cub = Math.Pow(number, 3); 
+        Console.WriteLine(cub);
+        number = number+2;                  
+    }
+    else
+    {
+        number++;
+    }    
+}
