50bbae5c5f047895cef6a4b08a50d0f084c9b55a
Anastasya <okayota83@mail.ru>
2022-10-08 20:08:18 +0400

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Введите N");
+int N = int.Parse(Console.ReadLine() ?? "0");
+int index = 1;
+long result = 1;
+
+while (index<=N)
+{
+    result = result*index;
+    index++;    
+}
+Console.Write(result);
