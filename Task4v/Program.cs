39abff67cd2dc9ca6127df5e39d5dae74a66cb18
Anastasya <okayota83@mail.ru>
2022-10-08 22:22:23 +0400

    add Task4v

---
+++ Task4v/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.WriteLine("Введите число a");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int digit = a%10;
+int cub = a*a*a;
+
+if (digit%2==0)
+{
+   Console.WriteLine("Число в кубе = " + cub); 
+}
+else 
+{
+   Console.WriteLine("Последняя цифра в числе нечетная"); 
+}
