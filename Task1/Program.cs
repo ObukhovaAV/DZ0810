3da069736e3a9d8b34cc3b2c5fab23a778852ef8
Anastasya <okayota83@mail.ru>
2022-10-08 19:42:04 +0400

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти кубы чисел от 1 до N
+Console.WriteLine("Введите N");
+int N = int.Parse(Console.ReadLine() ?? "0");
+int n = 1;
+
+
+Console.WriteLine("Кубы чисел от 1 до " + N);
+while (n<=N)
+{
+    int cub = n*n*n;    
+    Console.WriteLine(cub);
+    n++;
+}
